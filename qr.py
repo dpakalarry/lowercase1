e6f32b8f600eaa7f7eae352365cdce965146ecec
Michael <maspinwa@andrew.cmu.edu>
2020-01-09 11:35:11 -0500

    qr functions

--- qr.py
+++ qr.py
@@ -1,38 +1,40 @@
+from imutils.video import VideoStream
 from pyzbar import pyzbar
 import argparse
+import datetime
+import imutils
+import time
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-args = vars(ap.parse_args())
+def streamStart(pi=False):
+    # initialize the video stream and allow the camera sensor to warm up
+    print("[INFO] starting video stream...")
+    # vs = VideoStream(src=0).start()
+    vs = VideoStream(usePiCamera=pi).start()
+    time.sleep(2.0)
+    return vs
 
-# load the input image
-image = cv2.imread(args["image"])
- 
-# find the barcodes in the image and decode each of the barcodes
-barcodes = pyzbar.decode(image)
+def readQr(stream):
+    frame = stream.read()
+    barcodes = pyzbar.decode(frame)
+    if (len(barcodes) > 0):
+        for barcode in barcodes:
+            barcodeData = barcode.data.decode("utf-8")
+            barcodeType = barcode.type
+            # draw the barcode data and barcode type on the image
+            text = "{} ({})".format(barcodeData, barcodeType)
+            print(text)
 
-# loop over the detected barcodes
-for barcode in barcodes:
-	# extract the bounding box location of the barcode and draw the
-	# bounding box surrounding the barcode on the image
-	(x, y, w, h) = barcode.rect
-	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+def quitStream(stream):
+    stream.stop()
 
-	# the barcode data is a bytes object so if we want to draw it on
-	# our output image we need to convert it to a string first
-	barcodeData = barcode.data.decode("utf-8")
-	barcodeType = barcode.type
+vs = streamStart()
+while True:
+    readQr(vs)
+    key = cv2.waitKey(1) & 0xFF
 
-	# draw the barcode data and barcode type on the image
-	text = "{} ({})".format(barcodeData, barcodeType)
-	cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.5, (0, 0, 255), 2)
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+quitStream(vs)
 
-	# print the barcode type and data to the terminal
-	print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
-
-# show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
