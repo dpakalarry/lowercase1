4c3fe22adc31f7af63d668e3f3ed9fa9b7fc9036
Basaam Malik <basaammalik@gmail.com>
2020-01-09 17:44:38 -0500

    Added connection to Firebase server

--- android/app/src/main/java/com/summit/summitproject/AtmScheduleActivity.java
+++ android/app/src/main/java/com/summit/summitproject/AtmScheduleActivity.java
@@ -1,13 +1,20 @@
 package com.summit.summitproject;
 
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
@@ -26,6 +33,15 @@ public class AtmScheduleActivity extends AppCompatActivity {
     Button generate_QRCode;
     ImageView qrCode;
 
+    /**
+     * Used to persist user credentials if "Remember Me" is checked.
+     */
+    private SharedPreferences sharedPreferences;
+
+    /**
+     * The key under which the <b>username</b> will be stored in {@link SharedPreferences}.
+     */
+    private static final String PREF_USERNAME = "USERNAME";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,47 +51,62 @@ public class AtmScheduleActivity extends AppCompatActivity {
         qrCode=findViewById(R.id.imageView);
 
         generate_QRCode.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                JSONObject json = new JSONObject();
-                JSONObject user = new JSONObject();
-                JSONObject transaction = new JSONObject();
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-                try {
-                    user.put("account", "xxxxxxxxxxxx1234");
-                    user.put("balance", "20.00");
-                    user.put("name", "Rich Fairbanks");
+                sharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE);
+                String username = sharedPreferences.getString(PREF_USERNAME, "");
 
-                    transaction.put("transactionId", "xxxxxxxxxxxx5678");
-                    transaction.put("amount", "10.00");
-                    transaction.put("type", "Deposit");
+                DatabaseReference user = database.getReference("Usernames").child(username);
 
-                    TimeZone tz = TimeZone.getTimeZone("UTC");
-                    DateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
-                    date.setTimeZone(tz);
-                    String nowAsISO = date.format(new Date());
+                user.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String account = dataSnapshot.getValue().toString().substring(9, 17);
+                        Log.d("msg2", account);
 
-                    transaction.put("timestamp", nowAsISO);
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference bankAccounts= database.getReference("Bank Accounts");
+                        DatabaseReference balance = bankAccounts.child(account).child("Balance");
 
-                    json.put("user", user.toString());
-                    json.put("transaction", transaction.toString());
+                        JSONObject json = new JSONObject();
 
+                        try {
+                            json.put("acct", account);
+                            json.put("transId", "xxxx5678");
+                            json.put("amnt", "10.00");
+                            json.put("type", "d");
 
-                    String text=json.toString();
-                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-                    try {
-                        BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,500,500);
-                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-                        qrCode.setImageBitmap(bitmap);
+                            TimeZone tz = TimeZone.getTimeZone("UTC");
+                            DateFormat date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+                            date.setTimeZone(tz);
+                            String nowAsISO = date.format(new Date());
 
-                    } catch (WriterException e) {
-                        e.printStackTrace();
+                            json.put("time", nowAsISO);
+
+                            String text=json.toString();
+                            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                            try {
+                                BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,500,500);
+                                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                                qrCode.setImageBitmap(bitmap);
+
+                            } catch (WriterException e) {
+                                e.printStackTrace();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                });
             }
         });
     }
