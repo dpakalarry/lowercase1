d06c82f9c66f335f12b83dc9822396b3fb5b6e8d
Michael <maspinwa@andrew.cmu.edu>
2020-01-09 14:37:36 -0500

    QR Stuff

--- qr.py
+++ qr.py
@@ -1,10 +1,21 @@
 from imutils.video import VideoStream
 from pyzbar import pyzbar
-import argparse
+import json
 import datetime
 import imutils
 import time
 import cv2
+
+def convertFromJson(data):
+    print(data)
+    try:
+        dataDict = json.loads(data)
+    except ValueError as e:
+        return None
+    elements = ["user", "transaction"]
+    if all(elem in dataDict for elem in elements):
+        return dataDict
+    return None
 
 def streamStart(pi=False):
     # initialize the video stream and allow the camera sensor to warm up
@@ -17,22 +28,25 @@ def streamStart(pi=False):
 def readQr(stream):
     frame = stream.read()
     barcodes = pyzbar.decode(frame)
-    if (len(barcodes) > 0):
-        for barcode in barcodes:
-            barcodeData = barcode.data.decode("utf-8")
-            barcodeType = barcode.type
-            # draw the barcode data and barcode type on the image
-            text = "{} ({})".format(barcodeData, barcodeType)
-            print(text)
+    if len(barcodes) > 0:
+        barcode = barcodes.pop()
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        text = "{} ({})".format(barcodeData, barcodeType)
+        print(text)
+        return convertFromJson(barcodeData)
+    return None
 
 def quitStream(stream):
     stream.stop()
 
 vs = streamStart()
 while True:
-    readQr(vs)
+    data = readQr(vs)
+    if data != None:
+        print(data)
+        break
     key = cv2.waitKey(1) & 0xFF
-
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
