550c5f11df7118153e4d0fe831b245858be14608
Basaam Malik <basaammalik@gmail.com>
2020-01-09 20:07:18 -0500

    Prevented users from entering an amount if depositing

--- android/app/src/main/java/com/summit/summitproject/AtmScheduleActivity.java
+++ android/app/src/main/java/com/summit/summitproject/AtmScheduleActivity.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
