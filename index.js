d1b61181118301ab2191a1eb63a398a89a152e81
dpakalarry <devakusa@gmail.com>
2020-01-09 20:52:51 -0500

    adding js file for html page

---
+++ index.js
@@ -0,0 +1,16 @@
+function completeTransaction() {
+	var config = {
+		projectId: "atm-server-953c5",
+		//messagingSenderId: "678522738824"
+		apiKey: "AIzaSyCs_6BTTUoPm1zZJeB5-M6PoWozSbdATnA",
+		authDomain: "atm-server-953c5.firebaseapp.com",
+		databaseURL: "https://atm-server-953c5.firebaseio.com/",
+		storageBucket: "atm-server-953c5.appspot.com"
+	}
+	let fb = firebase.initializeApp(config);
+
+	console.log(fb.database()
+		// Get entire Db contents
+		.ref())
+
+}
