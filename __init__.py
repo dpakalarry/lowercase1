47dcb224595b7d4f64182551c863ed54584f606b
dpakalarry <devakusa@gmail.com>
2020-01-10 00:03:49 -0500

    pretty html

--- __init__.py
+++ __init__.py
@@ -9,36 +9,46 @@ import time
 
 stream = qr.streamStart()
 codeDict = None
-path = dir_path = os.getcwd()
 while True:
     codeDict = qr.readQr(stream)
     if codeDict != None:
+		
         transaction = backend.Transaction(codeDict["acct"], codeDict["amt"], codeDict["type"])
-        #webbrowser.open_new_tab(path+"/confirm.html"+"?act={}&amt={}&type={}&name={}".format(
-        #    codeDict["acct"], codeDict["amt"], codeDict["type"], transaction.correctName
-        #))
-		#TODO: Run the Node.js from here
+        site = open('confirm.html', 'r+')
+        html = "".join(site.readlines())
+        orig = html
+        html = html.replace('NameValue', transaction.correctName)
+        html = html.replace('TTValue', codeDict["type"])
+        html = html.replace('AccountValue',codeDict["acct"])
+        html = html.replace('AmountValue', codeDict["amt"])
+        print(html)
+        site.truncate(0)
+        site.write(html)
+        site.close()
+
+        filepath = 'file://' + os.path.realpath('confirm.html')
+        print(filepath)
+        webbrowser.open(filepath)
         codeDict = None
-        #time.sleep(15)
-        #if sys.platform.startswith('win'):
-        #    browserExe = "chrome.exe"
-        #    os.system("taskkill /f /im "+browserExe)
-        #elif sys.platform.startswith('linux'):
-        #    browserExe = "chrome"
-        #    os.system("pkill "+browserExe)
+        time.sleep(15)
+
+        site = open('confirm.html', 'w')
+        site.write(orig)
+        site.close()
+
 
 
             
             
-qr.quitStream(stream)
-qr.cv2.destroyAllWindows()
-if codeDict:
+#qr.quitStream(stream)
+#qr.cv2.destroyAllWindows()
+#if codeDict:
     
-    m = gui.Tk()
-    page = gui.confirmPage(transaction.correctName, codeDict["type"], codeDict["amount"], codeDict["account"])
-    page.genPage(m, transaction.execute)
-    m.mainloop()
-else:
+#    m = gui.Tk()
+#    page = gui.confirmPage(transaction.correctName, codeDict["type"], codeDict["amount"], codeDict["account"])
+#    page.genPage(m, transaction.execute)
+#    m.mainloop()
+#else:
     
-    messagebox.showerror("ERROR", "ERROR: {}".format("Exited"))
+#    messagebox.showerror("ERROR", "ERROR: {}".format("Exited"))
 
